9f7b66f72a4b4ea0fab8e449eefb132ea080c4bb
Sunil-RZT <sunil.s@razorthink.net>
2015-11-05 15:47:13 +0530

    Adding app icon

---
+++ AlarmNew/AlarmNew/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlarmNew
+//
+//  Created by Sunil Rao on 27/10/15.
+//  Copyright © 2015 Razorthink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AlarmNew/AlarmNew/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AlarmNew
+//
+//  Created by Sunil Rao on 27/10/15.
+//  Copyright © 2015 Razorthink. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
